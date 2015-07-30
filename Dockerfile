a08924a62248e8e963901a2f92c9981defc7b042
Matt Butcher <mbutcher@engineyard.com>
2015-07-30 13:04:25 -0600

    Initial config.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM scratch
 EXPOSE 5000
 ENV PORT 5000
-COPY server-linux server-linux
-ENTRYPOINT ["/server-linux"]
+COPY kubesnoop kubesnoop
+ENTRYPOINT ["/kubesnoop"]
--- server.go
+++ server.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
+const tpl = `<!doctype html>
+<html>
+<head>
+<title>KubeSnoop</title>
+</head>
+<body>
+
+<h2>Environment Variables</h2>
+<pre>{{.Env}}</pre>
+
+</body>
+</html>
+`
+
+var renderer *template.Template
+
 func main() {
-	http.HandleFunc("/", hello)
+	renderer = template.Must(template.New("html").Parse(tpl))
+
+	http.HandleFunc("/", snoop)
 
 	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	log.Println(addr)
@@ -17,6 +36,14 @@ func main() {
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello")
+func snoop(w http.ResponseWriter, r *http.Request) {
+
+	tvars := map[string]string{
+		"Env": "",
+	}
+
+	out, _ := exec.Command("env").CombinedOutput()
+	tvars["Env"] = string(out)
+
+	renderer.Execute(w, tvars)
 }
